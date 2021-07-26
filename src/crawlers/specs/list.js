02490f0547a6fc4a2a026e9ce1444893cfae45af
Jan Devos <jan@jandevos.org>
2021-07-26 15:51:39 +0800

    "Updated Xbox One and HLS Crawler"

--- src/crawlers/specs/list.js
+++ src/crawlers/specs/list.js
@@ -3,7 +3,7 @@ const GithubXMLPage = require('../xml/GithubXMLPage.js');
 
 const list = [
     {id: "av1Spec", link: 'https://github.com/AOMediaCodec/av1-spec/releases.atom', parser: GithubXMLPage, prettyName: "AV1 Specification"},
-    {id: "hlsSpec", link: 'https://tools.ietf.org/html/draft-pantos-hls-rfc8216bis', parser: hlsSpec, prettyName: "HLS"},
+    {id: "hlsSpec", link: 'https://datatracker.ietf.org/doc/html/draft-pantos-hls-rfc8216bis', parser: hlsSpec, prettyName: "HLS"},
     {id: "mediaSession", link: 'https://github.com/w3c/mediasession/commits/master.atom', parser: GithubXMLPage, prettyName: "Media Session"},
     {id: "mediaSourceExtensions", link: 'https://github.com/w3c/media-source/commits/gh-pages.atom', parser: GithubXMLPage, prettyName: "Media Source Extensions"},
     {id: "encryptedMediaExtensions", link: 'https://github.com/w3c/encrypted-media/commits/master.atom', parser: GithubXMLPage, prettyName: "Encrpted Media Extensions"},
